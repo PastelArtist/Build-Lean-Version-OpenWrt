0d745643b4aa08a24b95bdec750385e034d86ab7
Filtration <aocial@gmail.com>
2023-03-05 21:43:49 +0800

    Update diy-part2-Build-OpenWrt-v4-Xiaomi-Mi4A-Giga.sh

--- diy-part2-Build-OpenWrt-v4-Xiaomi-Mi4A-Giga.sh
+++ diy-part2-Build-OpenWrt-v4-Xiaomi-Mi4A-Giga.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ntp1.aliyun.com/time.pool.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.cloud.tencent.com/time.asia.apple.com/g' package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/cn.pool.ntp.org/g' package/base-files/files/bin/config_generate
