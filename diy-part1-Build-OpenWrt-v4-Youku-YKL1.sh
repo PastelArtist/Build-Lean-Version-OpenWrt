0730fae2709b0b7bd6c0fd29afee6b7256565440
Filtration <aocial@gmail.com>
2023-04-11 17:52:43 +0800

    Update diy-part1-Build-OpenWrt-v4-Youku-YKL1.sh

--- diy-part1-Build-OpenWrt-v4-Youku-YKL1.sh
+++ diy-part1-Build-OpenWrt-v4-Youku-YKL1.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1-Build-OpenWrt-v4-Xiaomi-Mi4A-Giga.sh
+# File name: diy-part1
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
